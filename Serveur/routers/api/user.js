1d8087d94cca374fc1ccc2dc442decc5bbb24988
CHEN Datao <chendatao1126@gmail.com>
2018-12-21 16:08:34 +0100

    creat user api

---
+++ Serveur/routers/api/user.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const router = express.Router();
+const User = require('../../models/User');
+
+router.get('/', (req, res) => {
+    User
+        .find()
+        .then(user => {
+            res.json(user);
+        })
+        .catch(e => {
+            res.json(e);
+        })
+})
+router.post('/', (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: req.body.password
+    });
+    newUser.save().then(user => {
+        res.json(user);
+    });
+});
+module.exports = router;
