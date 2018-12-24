d1601a316fc56950f88e20b0423479bb8a114ed3
CHEN Datao <chendatao1126@gmail.com>
2018-12-24 18:03:29 +0100

    Update MainActivity.java

--- EasyCommand/app/src/main/java/com/example/chendatao/myapplication/MainActivity.java
+++ EasyCommand/app/src/main/java/com/example/chendatao/myapplication/MainActivity.java
@@ -1,13 +1,23 @@
 package com.example.chendatao.myapplication;
 
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    Handler mHandle = new Handler();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mHandle.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+            }
+        },1000);
+
     }
 }
